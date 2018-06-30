216d57290baf365e776ebb645253cddaf82a3d70
Calle Lacouture <celacouture@gmail.com>
2018-06-30 11:57:37 -0500

    heroku

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,5 @@ module.exports={
   // mongoURI: "mongodb://carol:august3@ds121321.mlab.com:21321/overdont_db"
   // mongoURI:"mongodb://localhost/over_dont"
   // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/over_dont");
-  mongoURI: "mongodb://localhost/over_dont"
+  mongoURI: "mongodb://herokuhost/https://intense-sands-46199.herokuapp.com/"
 }
