5b1e34ff7d79403db76842a877a5cf5cdf183822
Calle Lacouture <celacouture@gmail.com>
2018-06-29 22:09:18 -0500

    components_added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>Hello</h1>
-      </div>
-    );
-  }
-}
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AdminForm from "./pages/AdminForm/AdminForm";
+import UserHome from "./pages/UserHome/UserHome";
+import AdminHome from "./pages/AdminHome/AdminHome";
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/AdminForm" component={AdminForm} />
+        <Route exact path="/UserHome" component={UserHome} />
+        <Route exact path="/AdminHome" component={AdminHome} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
